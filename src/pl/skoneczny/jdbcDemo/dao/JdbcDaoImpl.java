2dfcd8a9e2022eec560352bea3295aaedeb3be4f
Krzysztof Skoneczny <ksdragon@poczta.pl>
2018-09-26 11:35:10 +0200

    Dodano metodę getCircleName() w klasie JdbcDaoImpl

--- src/pl/skoneczny/jdbcDemo/dao/JdbcDaoImpl.java
+++ src/pl/skoneczny/jdbcDemo/dao/JdbcDaoImpl.java
@@ -65,6 +65,12 @@ public class JdbcDaoImpl {
     return jdbcTemplate.queryForInt(sql);
     }
     
+    
+    public String getCircleName(int circleId){
+        String sql = "SELECT NAME FROM CIRCLE WHERE ID = ?";        
+        return jdbcTemplate.queryForObject(sql,new Object[] {circleId}, String.class);
+    }
+    
     public DataSource getDataSource() {
         return dataSource;
     }
